568554120494e2ad54171fbde0c3fcd5ad2699c6
Simard302 <adamsimard@hotmail.com>
2024-06-14 09:11:14 -0400

    Made label example changes

--- selection/add_labels_to_selection_atoms/src/index.ts
+++ selection/add_labels_to_selection_atoms/src/index.ts
@@ -1,6 +1,7 @@
 import { Queries, QueryContext, StructureElement, StructureProperties, StructureSelection } from "molstar/lib/mol-model/structure";
 import { createRootViewer } from "./common/init";
 import { OrderedSet } from "molstar/lib/mol-data/int";
+import { StateTransforms } from "molstar/lib/mol-plugin-state/transforms";
 
 async function init() {
     // Create viewer
@@ -46,6 +47,9 @@ async function init() {
     // Since we iterated over residues and only selected 1 atom per residue, the selection is made of Singletons.
     const structure = (selection as StructureSelection.Singletons).structure;
     
+    // Create a StateBuilder to make changes to the state
+    const builder = plugin.build();
+    const dependsOn = [presetStateObjects.structure.ref]    // Define what state objects your changes depend on (in this case the structure SO)
     // Now, we will iterate over each Unit and its elements in the selection
     structure.units.forEach(unit => {
         for (let i = 0; i < unit.elements.length; i++) { // Iterate over each atom in the unit (each alpha carbon)
@@ -55,13 +59,21 @@ async function init() {
             // Create a location to retrieve the atom's compID (residue name)
             const location = StructureElement.Location.create(struct, unit, unit.elements[i]);
             const resName = StructureProperties.atom.auth_comp_id(location);
-            // Add label to the loci
-            plugin.managers.structure.measurement.addLabel(loci, {labelParams: {
-                // Set label parameters, in this case the text and size
-                customText: 'Alpha Carbon '+resName,
-                textSize: 1
-            }});
+            
+            // Create a MultiStructureSelection from our Loci
+            builder.toRoot().apply(StateTransforms.Model.MultiStructureSelectionFromExpression, {
+                selections: [
+                    { key: 'a', ref: presetStateObjects.structure.ref, expression: StructureElement.Loci.toExpression(loci) },
+                ],
+                label: 'Label'  // A label to give to our selection
+            }, {dependsOn}).apply(StateTransforms.Representation.StructureSelectionsLabel3D, { // Create a 3DLabel on the selection
+                customText: 'Alpha Carbon '+resName,    // Custom text for our label
+                textSize: 1     // Text size for our label
+            });
         }
     })
+    // Now that all the changes have been added to the builder,
+    // Commit them to be added to the current plugin state
+    builder.commit()
 }
 init();
